e04585a67c66f1ca2899513bb6fcd415c0e191fa
Jakub Tomaszewski <jakubto2001@gmail.com>
2023-06-09 13:58:28 +0200

    added functionalities for our project

--- home/views.py
+++ home/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 
+rooms = [
+    {'id': 1, 'name': "Stale zadania"},
+    {'id': 2, 'name': "Jednorazowe zadania"},
+    {'id': 3, 'name': "Kalendarz zadan"},
+    {'id': 4, 'name': "Generowanie raportow"},
+    {'id': 5, 'name': "Eksport raportow"},
+]
+
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'rooms':rooms})
 
 def room(request):
     return render(request, 'room.html')
