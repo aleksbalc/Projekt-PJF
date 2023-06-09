5dac777d994a539b0c3bfb097344a9b4a7cb1625
Jakub Tomaszewski <jakubto2001@gmail.com>
2023-06-09 18:01:53 +0200

    added 2 databases topics and messages

--- home/views.py
+++ home/views.py
@@ -1,23 +1,22 @@
 from django.shortcuts import render
+from .models import Room
 
 # Create your views here.
 
-rooms = [
-    {'id': 1, 'name': "Stale zadania"},
-    {'id': 2, 'name': "Jednorazowe zadania"},
-    {'id': 3, 'name': "Kalendarz zadan"},
-    {'id': 4, 'name': "Generowanie raportow"},
-    {'id': 5, 'name': "Eksport raportow"},
-]
+#rooms = [
+#    {'id': 1, 'name': "Stale zadania"},
+#    {'id': 2, 'name': "Jednorazowe zadania"},
+#    {'id': 3, 'name': "Kalendarz zadan"},
+#    {'id': 4, 'name': "Generowanie raportow"},
+#    {'id': 5, 'name': "Eksport raportow"},
+#]
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'home.html', context)
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'room.html', context)
