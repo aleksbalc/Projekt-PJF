435ad678466bd2048c17eb96b5b854c370a3be37
Jakub Tomaszewski <jakubto2001@gmail.com>
2023-06-09 19:18:44 +0200

    added create room form

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Room
+from .forms import RoomForm
 
 # Create your views here.
 
@@ -20,3 +21,8 @@ def room(request, pk):
     room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'room.html', context)
+
+def createRoom(request):
+    form = RoomForm()
+    context = {'form': form}
+    return render(request, 'room_form.html', context)
